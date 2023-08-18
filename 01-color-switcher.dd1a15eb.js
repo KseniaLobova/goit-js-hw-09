a54c1587977ae2cfd3a1abeb1f7cd8bddb87c064
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-08-18 12:47:14 +0000

    Deploying to gh-pages from @ KseniaLobova/goit-js-hw-09@174b3607919ea93480506fbde7298451d9b4729a 🚀

---
+++ 01-color-switcher.dd1a15eb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");console.log(o),e.addEventListener("click",(function(){colorIntervalId=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),colorIntervalId&&(e.disabled=!0,t.disabled=!1)})),t.addEventListener("click",(function(){clearInterval(colorIntervalId),e.disabled=!1,t.disabled=!0,console.log("stop")})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.dd1a15eb.js.map
